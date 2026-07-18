49d090a7732cc4feb54163943b70ab9865f46ffb
RussoPhone <100falecido@gmail.com>
2026-07-18 14:53:53 -0300

    new archtecure

--- app/services/pedido_service.py
+++ app/services/pedido_service.py
@@ -2,6 +2,16 @@ from app.schemas.pedido import Pedido
 
 pedidos = []
 
-def criar_pedido(pedido:Pedido):
-    pedidos.append(pedido)
-    return pedido
+class PedidoService(self, pedido):
+
+    def criar_pedido(pedido:Pedido):
+        pedidos.append(pedido)
+        return pedido
+
+    def alterar_status(self, id_pedido, novo_status):
+
+
+    def listar_pedidos(self):
+
+    def buscar_por_id(self, id_pedido):
+
--- pagamento.md
+++ pagamento.md
@@ -3,7 +3,6 @@ pagamento
 id
 forma de pagamento
 valor do pagamento
-validacao de pagamento
 
 status
 pendente
