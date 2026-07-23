14919c3a42301d7db96881536d04294ce079ee62
RussoPhone <100falecido@gmail.com>
2026-07-23 18:48:51 -0300

    change main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI
-from app.routes.pedidos import router as pedidos_router
+from fastapi import FastAPI 
 
-app = FastAPI()
+from app.routes.clientes import router as clientes_router 
+from app.routes.pedidos import router as pedidos_router 
 
-app.include_router(pedidos_router)
+app = FastAPI(title="Gerenciador de Pedidos")
+
+app.include_router(clientes_router) 
+app.include_router(pedidos_router) 
 
 @app.get("/")
 def home():
-    return{
-        "mensagem": "Gerenciador funcionando"
-    }
+    return {"mensagem": "Gerenciador funcionando"}
+
+
